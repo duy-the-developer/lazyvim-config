1162ddfe8a5ad40acb67e08d0063ec3f8a751e36
dan <dan@pop-os.hitronhub.home>
2024-08-18 22:20:36 -0400

    prelim config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,41 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- Increment/ decrement
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
+
+-- Delete a word backward
+-- keymap.set("n", "dw", "vb_d")
+
+-- Selet all
+keymap.set("n", "<C-A>", "gg<S-v>G")
+
+-- New tab
+keymap.set("n", "te", ":tabedit", opts)
+keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+keymap.set("n", "<S-tab>", ":tabprev<Return>", opts)
+
+-- Split window
+keymap.set("n", "ss", ":split<Return>", opts)
+keymap.set("n", "sv", ":vsplit<Return>", opts)
+
+-- Move window
+keymap.set("n", "<C-h>", "<C-w>h", opts)
+keymap.set("n", "<C-j>", "<C-w>j", opts)
+keymap.set("n", "<C-k>", "<C-w>k", opts)
+keymap.set("n", "<C-l>", "<C-w>l", opts)
+
+-- Resize window
+keymap.set("n", "<C-w><left>", "<C-w><", opts)
+keymap.set("n", "<C-w><right>", "<C-w>>", opts)
+keymap.set("n", "<C-w><down>", "<C-w>-", opts)
+keymap.set("n", "<C-w><up>", "<C-w>+", opts)
+
+-- Diagnostics
+-- keymap.set("n", "<C-j>", function()
+--   vim.diagnostic.goto_next()
+-- end, opts)
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,27 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local opt = vim.opt
+
+vim.g.mapleader = " "
+
+opt.encoding = "utf-8"
+opt.fileencoding = "utf-8"
+
+opt.number = true
+opt.relativenumber = true
+opt.ignorecase = true
+opt.wrap = false
+opt.showcmd = true
+opt.hlsearch = true
+opt.scrolloff = 8
+opt.sidescrolloff = 8
+opt.shell = "zsh"
+opt.backupskip = "/tmp/*,$TMPDIR/*,$TMP/*,$TEMP/*,$TEMPDIR/*"
+opt.backspace = { "start", "indent", "eol" }
+opt.path:append("**")
+opt.wildignore:append({ "*/node_modules/*" })
+opt.splitbelow = true
+opt.splitright = true
+opt.splitkeep = "cursor"
+opt.mouse = ""
