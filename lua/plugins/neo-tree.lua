f083c20adf09dd098706694de16fbd8e624b92ff
dan <duythedeveloper@gmail.com>
2024-09-29 21:50:58 -0400

    feat(neo-tree.lua): enable hidden files by default

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,10 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true, -- show filtered items (default = false)
+      },
+    },
+  },
+}
